6550906e356ec9a9e570326a78212145fb106ea8
ThiagoRocha <thiagormnb@gmail.com>
2025-05-18 17:18:55 -0300

    Fixing CORS problems

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { knapsackRoutes } from './routes/knapsack';
 
 const app = express();
 
+const corsOptions = {
+    origin: 'http://localhost:5173',
+}
+
 app.use(express.json());
+app.use(cors(corsOptions));
+
 app.use('/api', knapsackRoutes);
 
 app.listen(8080, () => 'server running on port 8080');
